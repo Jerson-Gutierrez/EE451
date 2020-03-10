40395a319e7d7b4a7e22771af92c971ca7c47603
Jerson Gutierrez <jersongu@usc.edu>
2020-03-09 18:02:25 -0700

    execution time added

--- pa4/ring.c
+++ pa4/ring.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <mpi.h>
 
 int main(int argc, char const *argv[])
 {
 	int npes, myrank;
-
+	struct timespec start, stop;
+ 	double time;
+ 	if( clock_gettime( CLOCK_REALTIME, &start) == -1 ) { perror( "clock gettime" );}
+ 	//need to run with 1, 2, 4, and 8 threads
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &npes);  // total number of processes
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
@@ -19,15 +23,15 @@ int main(int argc, char const *argv[])
 	} else if (myrank == 1) {
 		// recv from 1
 		MPI_Recv(&msg, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		printf("MSG = %d\n", msg);
 		msg++;
+		printf("MSG = %d\n", msg);
 		// send to 2
 		MPI_Send(&msg, 1, MPI_INT, 2, 1, MPI_COMM_WORLD);
 	} else if (myrank == 2) {
 		// recv from 2
 		MPI_Recv(&msg, 1, MPI_INT, 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
-		printf("MSG = %d\n", msg);
 		msg++;
+		printf("MSG = %d\n", msg);
 		// send to 3
 		MPI_Send(&msg, 1, MPI_INT, 3, 2, MPI_COMM_WORLD);
 	} else if (myrank == 3) {
@@ -37,5 +41,8 @@ int main(int argc, char const *argv[])
 		printf("MSG = %d\n", msg);
 	}
 	MPI_Finalize();
+	if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) { perror( "clock gettime" );}
+  	time = (stop.tv_sec - start.tv_sec)+ (double)(stop.tv_nsec - start.tv_nsec)/1e9;
+  	printf("Execution time = %f sec\n", time);
 	return 0;
 }
